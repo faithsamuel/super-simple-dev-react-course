389c21a12fa612c50ad11cfb19d9535b027d0af2
Faith Samuel <faithsamuel365@gmail.com>
2026-05-09 23:02:25 +0100

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import "./HomePage.css";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <link rel="icon" type="image/png" href="/images/home-favicon.png" />
