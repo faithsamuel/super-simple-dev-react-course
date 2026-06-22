afd1aa04f9d3ef588fb46486e77a151e1be1acd9
Faith Samuel <faithsamuel365@gmail.com>
2026-06-22 22:45:25 +0100

    Completed Exer 9 g-j

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -10,6 +10,8 @@ vi.mock("axios");
 describe("HomePage component", () => {
   let loadCart;
 
+  let user;
+
   beforeEach(() => {
     loadCart = vi.fn();
 
@@ -43,6 +45,7 @@ describe("HomePage component", () => {
         };
       }
     });
+    user = userEvent.setup();
   });
 
   it("displays the products correct", async () => {
@@ -64,5 +67,44 @@ describe("HomePage component", () => {
     expect(
       within(productContainers[1]).getByText("Intermediate Size Basketball"),
     ).toBeInTheDocument();
+  });
+
+  it("adds a product to the cart", async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>,
+    );
+    const productContainers = await screen.findAllByTestId("product-container");
+
+    const quantitySelector1 = within(productContainers[0]).getByTestId(
+      "product-quantity-selector",
+    );
+    await user.selectOptions(quantitySelector1, "2");
+
+    const addToCartButton1 = within(productContainers[0]).getByTestId(
+      "add-to-cart-button",
+    );
+    await user.click(addToCartButton1);
+
+    const quantitySelector2 = within(productContainers[1]).getByTestId(
+      "product-quantity-selector",
+    );
+    await user.selectOptions(quantitySelector2, "3");
+
+    const addToCartButton2 = within(productContainers[1]).getByTestId(
+      "add-to-cart-button",
+    );
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, "/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    });
+    expect(axios.post).toHaveBeenNthCalledWith(2, "/api/cart-items", {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 3,
+    });
+    expect(loadCart).toHaveBeenCalledTimes(2);
   });
 });
