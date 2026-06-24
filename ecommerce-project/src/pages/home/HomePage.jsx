2ca59ad77e40b91358606dd7d8106aad462d4ecc
Faith Samuel <faithsamuel365@gmail.com>
2026-06-24 22:21:33 +0100

    Created a new React Typescript project

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import { Header } from "../../components/Header";
 import "./HomePage.css";
 import { ProductsGrid } from "./ProductsGrid";
 
-export function HomePage({ cart, loadCart }) {
+export default function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
   const [searchParams] = useSearchParams();
   const search = searchParams.get("search");
